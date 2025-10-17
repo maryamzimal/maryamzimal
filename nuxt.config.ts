b90fda427025a0f2d891e8baac0a01670ab9b907
maryamzimal <maryamzimal24@gmail.com>
2025-10-17 16:01:28 +0500

    version 0.0.0

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxthub/core',
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  ui: { content: true },
+  compatibilityDate: '2025-07-15',
+  eslint: { config: { stylistic: true } },
+})
